75c8ef3a3b2c1a2d1219356813c6c8f680051db6
Carter Hay <clccmh@gmail.com>
2016-08-31 20:59:49 -0500

    began playing with phaser

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+
+window.onload = function() {
+
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'Carters Carts', { preload: preload, create: create });
+
+  function preload () {
+
+    game.load.image('logo', 'assets/phaser.png');
+    game.load.bitmapFont('carrier_command', '../assets/fonts/bitmapFonts/carrier_command.png', '../assets/fonts/bitmapFonts/carrier_command.xml');
+
+  }
+
+  function create () {
+
+    //var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    //logo.anchor.setTo(0.5, 0.5);
+
+    bmpText = game.add.bitmapText(10, 100, 'carrier_command', "Welcome to Carter's Cars", 34);
+    bmpText.inputEnabled = true;
+  }
+
+};
