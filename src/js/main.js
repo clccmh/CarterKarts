47c378f084870c15f1122473b9a161366cfd7131
Carter Hay <clccmh@gmail.com>
2016-08-31 23:30:19 -0500

    added fonts, working text render

--- src/js/main.js
+++ src/js/main.js
@@ -6,17 +6,17 @@ window.onload = function() {
   function preload () {
 
     game.load.image('logo', 'assets/phaser.png');
-    game.load.bitmapFont('carrier_command', '../assets/fonts/bitmapFonts/carrier_command.png', '../assets/fonts/bitmapFonts/carrier_command.xml');
+    game.load.bitmapFont('carrier_command', 'assets/fonts/bitmapFonts/carrier_command.png', 'assets/fonts/bitmapFonts/carrier_command.xml');
 
   }
 
   function create () {
 
-    //var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    //logo.anchor.setTo(0.5, 0.5);
+    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
 
-    bmpText = game.add.bitmapText(10, 100, 'carrier_command', "Welcome to Carter's Cars", 34);
-    bmpText.inputEnabled = true;
+    bmpText = game.add.bitmapText(10, 100, 'carrier_command', "Carter's Carts", 34);
+    //bmpText.inputEnabled = true;
   }
 
 };
